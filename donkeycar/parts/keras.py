0656898c14099f105f82945dd481cc6ce606b103
Sence <991693552@qq.com>
2019-05-13 16:54:18 +0800

    keras modify

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -57,7 +57,7 @@ class KerasPilot:
             verbose=1,
             validation_data=val_gen,
             callbacks=callbacks_list,
-            validation_steps=int(steps * (1.0 - train_split) // train_split))
+            validation_steps=int(steps * (1.0 - train_split) / train_split))
         return hist
 
 
